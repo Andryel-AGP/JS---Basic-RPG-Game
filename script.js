c93ea1bdca4deecaf3cf311946a73bbabc43499b
Andryel de Paula <121632051+Andryel-AGP@users.noreply.github.com>
2024-05-13 17:04:11 -0300

    Update script.js
    Update:
    Locations: win
    text.innerHTML
    function winGame

--- script.js
+++ script.js
@@ -75,6 +75,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You die. &#x2620;"
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
   }
 ];
 
@@ -91,7 +97,7 @@ function update(location) {
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
-  text.innerText = location.text;
+  text.innerHTML = location.text;
 }
 
 function goTown() {
@@ -204,6 +210,10 @@ function defeatMonster() {
 
 function lose() {
   update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6])
 }
 
 function restart() {
